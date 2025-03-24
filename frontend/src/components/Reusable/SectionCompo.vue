68b607ea5b368e2c2800918e24a339ca21b5a8f0
MiKL-B <77576674+MiKL-B@users.noreply.github.com>
2025-03-24 10:53:25 +0100

    Update SectionCompo.vue

--- frontend/src/components/Reusable/SectionCompo.vue
+++ frontend/src/components/Reusable/SectionCompo.vue
@@ -1,1 +1,14 @@
 
+<template>
+  <article>
+    <section class="py-32 px-6 md:max-w-7xl lg:mx-auto">
+      <slot name="content"></slot>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "SectionCompo",
+};
+</script>
